8e4f875124eff75406f984103e58aafd5f0ada53
Jenna Howieson <jenna@jennahowieson.co.uk>
2023-01-12 12:18:59 +0000

    Updates

---
+++ precache-manifest.c9b82bea52fa1a33b1a2434d9fc64cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b410fb06b82d867ca93",
+    "url": "/portfolio/static/css/main.584adef9.chunk.css"
+  },
+  {
+    "revision": "5b410fb06b82d867ca93",
+    "url": "/portfolio/static/js/main.5b410fb0.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/portfolio/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "9fe87b63dd6776d194d3",
+    "url": "/portfolio/static/css/2.5016b5f2.chunk.css"
+  },
+  {
+    "revision": "9fe87b63dd6776d194d3",
+    "url": "/portfolio/static/js/2.9fe87b63.chunk.js"
+  },
+  {
+    "revision": "f800b1316a521fadcf37",
+    "url": "/portfolio/static/js/runtime~main.f800b131.js"
+  },
+  {
+    "revision": "0f281f98bb42f00b0958362cb09afae6",
+    "url": "/portfolio/index.html"
+  }
+];
