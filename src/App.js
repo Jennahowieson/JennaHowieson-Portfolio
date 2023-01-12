d6639927d6f47a956f007a1af0b8d5cb421af9c3
Jenna Howieson <jenna@jennahowieson.co.uk>
2023-01-12 13:42:00 +0000

    change home route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/portfolio" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
